69ac8385ec373c330a9fa3d4e2ed73f76f4244e0
hcyspeech <aaxxdd74@gmail.com>
2022-09-15 15:32:07 +0900

    [Update] Readme

--- utils.py
+++ utils.py
@@ -135,9 +135,14 @@ def load_wav_to_torch(full_path):
   return torch.FloatTensor(data.astype(np.float32)), sampling_rate
 
 
-def load_filepaths_and_text(filename, split="|"):
+def load_filepaths_and_text(data_path, filename, split="|"):
+  filepaths_and_text = []
   with open(filename, encoding='utf-8') as f:
-    filepaths_and_text = [line.strip().split(split) for line in f]
+    for line in f:
+      directory, text, sid = line.strip().split(split)
+      wav = os.path.join(data_path,sid,directory+'.wav')
+      filepaths_and_text.append([wav, sid, text])
+      # print(wav, sid, text)
   return filepaths_and_text
 
 
